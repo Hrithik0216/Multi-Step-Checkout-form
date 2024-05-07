a68acdb69900f47882d812f8b5072e58b05e5d7e
Hrithik Arun Kumar M <128972805+Hrithik0216@users.noreply.github.com>
2024-05-07 23:23:28 +0530

    Added phone number input field

--- src/components/steps/Account.jsx
+++ src/components/steps/Account.jsx
@@ -55,6 +55,21 @@ const Account = () => {
           />
         </div>
       </div>
+
+      <div className="mx-2 w-full flex-1">
+        <div className="mt-3 h-6 text-xs font-bold uppercase leading-8 text-gray-500">
+          Phone Number
+        </div>
+        <div className="my-2 flex rounded border border-gray-200 bg-white p-1">
+          <input
+            onChange={handleChange}
+            value={userData["phone"] || ""}
+            name="phone"
+            placeholder="Phone Number"
+            className="w-full appearance-none p-1 px-2 text-gray-800 outline-none"
+          />
+        </div>
+      </div>
     </div>
   );
 };
